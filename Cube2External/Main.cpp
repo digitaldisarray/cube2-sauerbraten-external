efc930b291506ff46c5456e470ebc3278e86d519
Kevin <electrichexagon8@gmail.com>
2021-07-27 13:44:44 -0700

    ESP

--- Cube2External/Main.cpp
+++ Cube2External/Main.cpp
@@ -11,11 +11,13 @@
 #include "dataTypes.h"
 #include "Helpers.h"
 #include "offsets.h"
+#include "gltext.h"
 
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
+
 
 int main() {
 
@@ -28,24 +30,15 @@ int main() {
         return 0;
     }
 
-    // get the base address of the game
-    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"sauerbraten.exe"); // 7FF6768C0000 - sauerbraten.exe
-    std::cout << "Module Base: 0x" << std::hex << moduleBase << std::endl;
+    // Get the base address of the game
+    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"sauerbraten.exe");
 
     // Open handle to process
     HANDLE hProcess = 0;
     hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
-    std::cout << "hProcess: " << hProcess << std::endl;
 
     localPlayer.dmaHealth = FindDMAAddy(hProcess, (moduleBase + localPlayer.base), { 0x0, localPlayer.health });
-
-    uintptr_t playerListAddr = 0;
-
-    //You must read the pointer to get the address of it here
-    //ReadProcessMemory(hProcess, (BYTE*)(moduleBase + general.playerList), &playerListAddr, sizeof(playerListAddr), NULL);
-    //std::cout << "0x" << std::hex << (moduleBase + general.playerList) << std::endl;
-    //std::cout << "0x" << std::hex << playerListAddr << std::endl;
-
+    std::cout << "among us: 0x" << std::hex << localPlayer.dmaHealth << std::endl;
 
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
@@ -64,6 +57,7 @@ int main() {
     glfwWindowHint(GLFW_RESIZABLE, false);
     glfwWindowHint(GLFW_MAXIMIZED, true);
     glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, true);
+
 
     // Create window with graphics context
     GLFWwindow* window = glfwCreateWindow(monitorWidth, monitorHeight, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
@@ -95,6 +89,7 @@ int main() {
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
 
+
     // Setup Dear ImGui style
     //ImGui::StyleColorsDark();
     ImGui::StyleColorsClassic();
@@ -103,7 +98,16 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
+    // Temporary setting vars
     bool bMenuVisible = true;
+    bool bESP = true;
+    bool bTracers = true;
+    bool bTeamCheck = true;
+    bool bGodMode = false;
+    
+    // GL Font setup
+    GL::Font font;
+    font.Build(12); // TODO: make scale to screen resolution
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
@@ -132,28 +136,80 @@ int main() {
             int numPlayers = 0;
             ReadProcessMemory(hProcess, (BYTE*)(moduleBase + general.numPlayers), &numPlayers, sizeof(numPlayers), nullptr);
             
+            if (bGodMode) {
+                int amount = 333;
+                // Real health
+                WriteProcessMemory(hProcess, (LPVOID)localPlayer.dmaHealth, &amount, sizeof(amount), nullptr);
+
+                // Display value (todo)
+            }
+
+            // If other players exist
             if (numPlayers > 1) {
                 Matrix4D projectionMatrix;
                 ReadProcessMemory(hProcess, (BYTE*)(moduleBase + general.projMatrix), &projectionMatrix, sizeof(projectionMatrix), nullptr);
 
+
                 // Go to each player
                 for (int i = 1; i < numPlayers; i++) {
-                    uintptr_t posAddr = FindDMAAddy(hProcess, (moduleBase + general.playerList), { (unsigned int)(i * 8), 0x30 });
-                    Vec3 position;
-                    ReadProcessMemory(hProcess, (BYTE*)(posAddr), &position, sizeof(position), nullptr);
+                    uintptr_t healthAddr = FindDMAAddy(hProcess, (moduleBase + general.playerList), { (unsigned int)(i * 8), entity.health });
+                    int health = 0;
+                    ReadProcessMemory(hProcess, (BYTE*)healthAddr, &health, sizeof(health), nullptr);
 
-                    Vec2 screenCoords;
-                    if (!WorldToScreen(position, screenCoords, projectionMatrix.Matrix, 1920, 1080)) {
+                    // Skip dead entities
+                    if (health <= 0) {
                         continue;
                     }
 
-                    //std::cout << screenCoords.X << std::endl;
-                    //std::cout << screenCoords.Y << std::endl;
+                    // Read position
+                    uintptr_t posAddr = FindDMAAddy(hProcess, (moduleBase + general.playerList), { (unsigned int)(i * 8), entity.position });
+                    Vec3 position;
+                    ReadProcessMemory(hProcess, (BYTE*)(posAddr), &position, sizeof(position), nullptr);
 
-                    glBegin(GL_LINES);
-                    glVertex2f(0.f, -1.f);
-                    glVertex2f(screenCoords.X, screenCoords.Y);
-                    glEnd();
+                    Vec3 topPosition = position;
+                    topPosition.Z += 4;
+
+                    Vec3 bottomPosition = position;
+                    bottomPosition.Z -= 14;
+
+                    if (bTracers) {
+                        Vec2 screenCoords;
+                        if (!WorldToScreen(topPosition, screenCoords, projectionMatrix.Matrix, 1920, 1080)) {
+                            continue;
+                        }
+
+                        glBegin(GL_LINES);
+                        glVertex2f(0.f, -1.f);
+                        glVertex2f(screenCoords.X, screenCoords.Y);
+                        glEnd();
+
+
+                        if (!WorldToScreen(bottomPosition, screenCoords, projectionMatrix.Matrix, 1920, 1080)) {
+                            continue;
+                        }
+
+                        glBegin(GL_LINES);
+                        glVertex2f(0.f, -1.f);
+                        glVertex2f(screenCoords.X, screenCoords.Y);
+                        glEnd();
+                    }
+
+                    if (bESP) {
+
+                        Vec2 screenCoords;
+                        if (!WorldToScreen(topPosition, screenCoords, projectionMatrix.Matrix, 1920, 1080)) {
+                            continue;
+                        }
+
+                        // Read entity name (max length is 15 chars)
+                        uintptr_t nameAddr = FindDMAAddy(hProcess, (moduleBase + general.playerList), { (unsigned int)(i * 8), entity.name });
+                        char name[15];
+                        ReadProcessMemory(hProcess, (BYTE*)nameAddr, &name, sizeof(name), nullptr);
+
+
+                        font.Print(screenCoords.X, screenCoords.Y, 255, 255, 255, name);
+                    }
+
                 }
             }
         }
@@ -163,6 +219,10 @@ int main() {
 
             ImGui::Begin("x86Cheats - Cube 2: Sauerbraten");
             
+
+            ImGui::Checkbox("Tracers", &bTracers);
+            ImGui::Checkbox("God Mode (Offline Only)", &bGodMode);
+
 
             if (ImGui::Button("Exit")) {
                 return 0;
--- Cube2External/offsets.h
+++ Cube2External/offsets.h
@@ -7,11 +7,22 @@ struct LocalPlayer {
     const unsigned int xPos = 0x394;
     const unsigned int yPos = 0x398;
     const unsigned int zPos = 0x39C;
+    const unsigned int viewAngles = 0x3C;
     uintptr_t dmaHealth = 0x0;
     uintptr_t dmaXPos = 0x0;
     uintptr_t dmaYPos = 0x0;
     uintptr_t dmaZPos = 0x0;
 }localPlayer;
+
+
+struct Entity {
+    const unsigned int position = 0x30; // 0x30 is x, 0x34 is y, 0x38 is z
+    const unsigned int alive = 0x77;
+    const unsigned int health = 0x178;
+    const unsigned int armor = 0x180;
+    const unsigned int name = 0x274;
+    const unsigned int team = 0x378;
+}entity;
 
 struct General {
     const unsigned int playerList = 0x346C90;
